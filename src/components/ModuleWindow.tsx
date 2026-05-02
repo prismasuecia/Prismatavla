33002488bb2783670b4a069e9fadb32efb7b57bc
Prismasuecia <info@joellindhe.com>
2026-05-02 10:49:11 +0200

    ModuleWindow: enableUserSelectHack=false för react-rnd Safari fix

--- src/components/ModuleWindow.tsx
+++ src/components/ModuleWindow.tsx
@@ -121,6 +121,7 @@ export function ModuleWindow({ layout, config, children, chipLabel }: ModuleWind
 
   return (
     <Rnd
+        enableUserSelectHack={false}
       className={clsx('module-window', { 'is-active': isActive, 'is-locked': isLocked })}
       style={{ zIndex: layout.zIndex }}
       size={currentSize}
